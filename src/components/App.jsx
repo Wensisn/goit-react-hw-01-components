5f1df08aa042dee72e63a35e7b544f1223c5fbf5
Zubenko1 <zubenkodp@gmail.com>
2022-10-06 16:26:30 +0300

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+import { Profile } from "./Profile/Profile";
+import user from '../'
 export const App = () => {
   return (
     <div
@@ -10,7 +12,7 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <Profile users={user} />
     </div>
   );
 };
