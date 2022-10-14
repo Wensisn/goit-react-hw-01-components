a8c6bb89f5b197145874b8191cda66730c9f9c10
Zubenko1 <zubenkodp@gmail.com>
2022-10-14 03:29:06 +0300

    Last change

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -33,5 +33,9 @@ Profile.propTypes = {
   username: PropTypes.string.isRequired,
   tag: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
-  stats: PropTypes.object.isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }).isRequired,
 };
